f8a3d65494bb58bacacefba9653362f9575edf99
Joseph Davidson <j.davidson18@gmail.com>
2023-08-07 08:28:59 -0600

    initial commit

---
+++ src/main/java/com/jgdBoot/eCommBoot/ECommBootApplication.java
@@ -0,0 +1,18 @@
+package com.jgdBoot.eCommBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class ECommBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommBootApplication.class, args);
+	}
+
+
+
+}
